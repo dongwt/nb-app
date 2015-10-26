050a68cbb0a6e811dbfcac58b4d25787694da0a3
dongwt <834575475@qq.com>
2015-10-26 18:05:56 +0800

    init

--- app/orders/ordersCtrl.js
+++ app/orders/ordersCtrl.js
@@ -6,6 +6,14 @@ define(['app'], function (app) {
   app.registerController("ordersCtrl",
     ["$rootScope","$scope",function($rootScope,$scope){
 
+
+      $scope.$on('$ionicView.beforeEnter',
+        function () {
+          $scope.user = $rootScope.CacheFactory.get("NB-USER");
+
+        })
+
+
       function execute(){
         $scope.user = $rootScope.CacheFactory.get("NB-USER");
       }
